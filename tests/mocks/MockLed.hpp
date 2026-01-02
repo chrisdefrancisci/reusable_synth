bf52988713684a48d85cbd3e318a74f7da9455ed
Chris DeFrancisci <defranciscic@trxsystems.com>
2026-01-02 15:51:06 -0500

    Tests + mock super simple example, export compile commands

--- tests/mocks/MockLed.hpp
+++ tests/mocks/MockLed.hpp
@@ -5,5 +5,10 @@
 
 class MockLed : public ledBase
 {
+    public:
     MOCK_METHOD(void, on, (), (override));
+    MOCK_METHOD(void, off, (), (override));
+    MOCK_METHOD(void, setIntensity, (int value), (override));
+    MOCK_METHOD(void, setIntensity, (float value), (override));
+    MOCK_METHOD((std::pair<int, int>), getRange, (), (const, override));
 };
--- tests/tests/led_test.cpp
+++ tests/tests/led_test.cpp
@@ -1,6 +1,31 @@
+#include "gmock/gmock.h"
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <tests/mocks/MockLed.hpp>
+
+using ::testing::AtLeast;
+using ::testing::AtMost;
+using ::testing::Return;
 
 TEST(LedTest, test1)
 {
     ASSERT_EQ(1, 1);
+}
+
+TEST(LedTest, test2)
+{
+    MockLed led;
+
+    EXPECT_CALL(led, on()).Times(AtLeast(1));
+    EXPECT_CALL(led, off()).Times(AtMost(0));
+    EXPECT_CALL(led, getRange())
+        .Times(AtLeast(1))
+        .WillRepeatedly(Return(std::pair<int, int>(0, 1)));
+    
+    led.on();
+
+    auto range = led.getRange();
+    EXPECT_EQ(range.first, 0);
+    EXPECT_EQ(range.second, 1);
 }
