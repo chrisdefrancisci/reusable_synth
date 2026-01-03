633961d09a0ebbdd3cbe6087563384daf28565e3
Chris DeFrancisci <chrisdefrancisci@gmail.com>
2026-01-02 19:32:30 -0500

    Noncopyable base class

---
+++ reusable_synth/Utils/noncopyable.hpp
@@ -0,0 +1,25 @@
+
+ #pragma once
+
+/**
+ * @brief A base class to prohibit copying (via assignment or construction).
+ * 
+ * From "Real Time C++" by Christopher Kormanyos, which bases it's example on Boost Noncopyable.
+ * https://github.com/ckormanyos/real-time-cpp/blob/master/ref_app/src/util/utility/util_noncopyable.h
+ *
+ * Distributed under the Boost Software License,
+ * Version 1.0. (See accompanying file LICENSE_1_0.txt
+ * or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+class noncopyable
+{
+protected:
+    noncopyable() {}
+    ~noncopyable() {}
+private:
+    noncopyable(const noncopyable&) = delete;
+    noncopyable(noncopyable&&)      = delete;
+
+    auto operator=(const noncopyable&) -> noncopyable& = delete;
+    auto operator=(noncopyable&&)      -> noncopyable& = delete;
+};
