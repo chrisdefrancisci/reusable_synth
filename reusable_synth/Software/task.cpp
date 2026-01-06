74226fe67423903fd51ebf816f6d1be863a3b3ca
Chris DeFrancisci <chrisdefrancisci@gmail.com>
2026-01-05 19:32:36 -0500

    compile commands now generated and copied (for language server), formatting

--- reusable_synth/Software/task.cpp
+++ reusable_synth/Software/task.cpp
@@ -5,14 +5,15 @@ extern "C"
     volatile uint32_t g_system_tick_count = 0;
 }
 
-template <typename TickType>
+template<typename TickType>
 void timer<TickType>::start_interval(tick_type interval)
 {
-    start_tick = static_cast<TickType>(g_system_tick_count); // set start tick to "now"
-    interval_ticks = interval;                               // set interval "n ticks from now"
+    start_tick =
+      static_cast<TickType>(g_system_tick_count); // set start tick to "now"
+    interval_ticks = interval; // set interval "n ticks from now"
 }
 
-template <typename TickType>
+template<typename TickType>
 bool timer<TickType>::timeout() const
 {
     const tick_type current = static_cast<TickType>(g_system_tick_count);
@@ -22,8 +23,7 @@ bool timer<TickType>::timeout() const
 
 bool task_control_block::execute()
 {
-    if (time.timeout())
-    {
+    if (time.timeout()) {
         time.start_interval(cycle); // restart timer for 'cycle' ticks from now
         function();
 
@@ -49,28 +49,27 @@ void task_c()
     // TODO: Send telemetry
 }
 
-std::array<task_control_block, 3> task_list = {task_control_block(task_a,
-                                                                  task_control_block::timer_type::milliseconds(2),
-                                                                  task_control_block::timer_type::milliseconds(0)),
+std::array<task_control_block, 3> task_list = {
+    task_control_block(task_a,
+                       task_control_block::timer_type::milliseconds(2),
+                       task_control_block::timer_type::milliseconds(0)),
 
-                                               task_control_block(task_b,
-                                                                  task_control_block::timer_type::milliseconds(8),
-                                                                  task_control_block::timer_type::milliseconds(7)),
+    task_control_block(task_b,
+                       task_control_block::timer_type::milliseconds(8),
+                       task_control_block::timer_type::milliseconds(7)),
 
-                                               task_control_block(task_c,
-                                                                  task_control_block::timer_type::milliseconds(16),
-                                                                  task_control_block::timer_type::milliseconds(13))};
+    task_control_block(task_c,
+                       task_control_block::timer_type::milliseconds(16),
+                       task_control_block::timer_type::milliseconds(13))
+};
 
 void scheduler()
 {
-    for (;;)
-    {
+    for (;;) {
         // Find first ready task using std::find_if
-        std::find_if(task_list.begin(),
-                     task_list.end(),
-                     [](task_control_block &tcb) -> bool
-                     {
-                         return tcb.execute();
-                     });
+        std::find_if(
+          task_list.begin(),
+          task_list.end(),
+          [](task_control_block& tcb) -> bool { return tcb.execute(); });
     }
 }
