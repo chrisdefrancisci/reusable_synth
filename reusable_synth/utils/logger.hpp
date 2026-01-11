86a7689bde7e8580f968fafe8f7c450be365050c
Chris DeFrancisci <chrisdefrancisci@gmail.com>
2026-01-10 22:01:10 -0500

    Logger implementation

--- reusable_synth/utils/logger.hpp
+++ reusable_synth/utils/logger.hpp
@@ -7,31 +7,187 @@
 
 #include <cstring>
 #include <iostream>
+#include <source_location>
+#include <sstream>
 #include <string_view>
 
 #include <reusable_synth/utils/ring_buffer.hpp>
 
-template<int nLogs, int logLen>
-class Logger {
-public:
-    Logger();
+/**
+ * @brief Enumerates log severity
+ *
+ */
+enum class LogType
+{
+    INFO,
+    WARNING,
+    ERROR
+};
 
-    void aadf();
+/**
+ * @brief Holds a string with a LogType enum.
+ *
+ * @tparam logLen The number of characters (including terminator) a log holds.
+ */
+template<int logLen>
+class Log
+{
+public:
+    Log()
+      : logType(LogType::INFO)
+    {
+        memset(buff, '\0', logLen);
+    }
+
+    Log(LogType type, const char* message)
+      : logType(type)
+    {
+        if (strlen(message) < logLen) {
+            strcpy(buff, message);
+        } else {
+            memcpy(buff, message, logLen - 1);
+            buff[logLen - 1] = '\0';
+        }
+    }
+
+    Log(const Log& other)
+      : logType(other.logType)
+    {
+        memcpy(buff, other.buff, logLen);
+    }
+
+    Log(Log&& other)
+      : logType(other.logType)
+    {
+        memcpy(buff, other.buff, logLen);
+    }
+
+    Log& operator=(const Log& other)
+    {
+        logType = other.logType;
+        memcpy(buff, other.buff, logLen);
+        return *this;
+    }
+
+    Log& operator=(const Log&& other)
+    {
+        logType = other.logType;
+        memcpy(buff, other.buff, logLen);
+        return *this;
+    }
+
+    const char* pBuffer() const { return buff; };
+    LogType type() const { return logType; };
 
 private:
-    class Log {
-    public:
-        Log()
-          : type(LogType::INFO) {
-            memset(buff, '\0', logLen);
-        };
-        const char* pBuffer() { return buff; };
+    char buff[logLen];
+    LogType logType;
+};
 
-    private:
-        enum class LogType { INFO, WARNING, ERROR };
-        char buff[logLen] = { 0 };
-        LogType type;
-    };
+/**
+ * @brief A class to store the most recent logs in a ring buffer.
+ *
+ * @tparam nLogs The number of logs to hold.
+ * @tparam logLen The length of each log.
+ */
+template<int nLogs, int logLen>
+class Logger
+{
+public:
+    Logger()
+      : fullFlag(false)
+    {
+    }
 
-    RingBuffer<Log, nLogs> logs;
+    /**
+     * @brief Convenience function to add a info message to the logger.
+     *
+     * @param message The log message.
+     * @param location The location associated with the message. Default reports
+     * caller location.
+     */
+    void info(
+      const std::string_view message,
+      const std::source_location location = std::source_location::current())
+    {
+        log(LogType::INFO, message, location);
+    }
+
+    /**
+     * @brief Convenience function to add a warning to the logger.
+     *
+     * @param message The log message.
+     * @param location The location associated with the message. Default reports
+     * caller location.
+     */
+    void warn(
+      const std::string_view message,
+      const std::source_location location = std::source_location::current())
+    {
+        log(LogType::WARNING, message, location);
+    }
+
+    /**
+     * @brief Convenience function to add an error to the logger.
+     *
+     * @param message The log message.
+     * @param location The location associated with the message. Default reports
+     * caller location.
+     */
+    void error(
+      const std::string_view message,
+      const std::source_location location = std::source_location::current())
+    {
+        log(LogType::ERROR, message, location);
+    }
+
+    /**
+     * @brief Adds a log to the logger.
+     *
+     * @remarks If the buffer is full, a warning log will be added.
+     *
+     * @param type The log severity.
+     * @param message The log message.
+     * @param location The location associated with the message. Default reports
+     * caller location.
+     */
+    void log(
+      LogType type,
+      const std::string_view message,
+      const std::source_location location = std::source_location::current())
+    {
+        // TODO: find alternative solution, stringstream is likely a terrible
+        // choice for a memory constrained system
+        std::stringstream ss;
+        if (logsBuffer.full()) {
+            fullFlag = true;
+            ss << std::source_location::current().file_name() << ":"
+               << std::source_location::current().line() << " "
+               << "Logger overflow!";
+
+            logsBuffer.push_back(
+              Log<logLen>(LogType::WARNING, ss.str().c_str()));
+            ss.clear();
+            ss.str("");
+        }
+        ss << location.file_name() << ":" << location.line() << " " << message;
+
+        logsBuffer.push_back(Log<logLen>(type, ss.str().c_str()));
+    }
+
+    /**
+     * @brief Removes the oldest log from the logger.
+     *
+     * @return std::optional<Log<logLen>> The removed log if there was one or
+     * std::nullopt.
+     */
+    std::optional<Log<logLen>> remove_log()
+    {
+        fullFlag = false;
+        return logsBuffer.pop_front();
+    }
+
+private:
+    RingBuffer<Log<logLen>, nLogs> logsBuffer;
+    bool fullFlag;
 };
---
+++ tests/tests/utils/logger_test.cpp
@@ -0,0 +1,79 @@
+#include <cstring>
+#include <source_location>
+#include <sstream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include <reusable_synth/utils/logger.hpp>
+
+TEST(LoggerTest, StringTooLong) {}
+
+TEST(LoggerTest, AddOneString) {
+  constexpr int nLogs = 10;
+  constexpr int logLen = 200;
+  Logger<nLogs, logLen> logger;
+  std::stringstream expectedVal;
+  expectedVal << std::source_location::current().file_name() << ":"
+              << (std::source_location::current().line() + 1) << " test";
+  logger.info("test");
+  auto val = logger.remove_log();
+
+  ASSERT_TRUE(val.has_value());
+  EXPECT_STREQ(val->pBuffer(), expectedVal.str().c_str());
+  EXPECT_EQ(val->type(), LogType::INFO);
+
+  //   std::cout << val->pBuffer() << std::endl;
+  //   std::cout << expectedVal.str() << std::endl;
+}
+
+TEST(LoggerTest, StringOverflow) {
+  constexpr int nLogs = 10;
+  constexpr int logLen = 200;
+  Logger<nLogs, logLen> logger;
+
+  char overflowChars[logLen];
+  memset(overflowChars, 'a', logLen);
+  overflowChars[logLen - 1] = '\0';
+
+  logger.info(overflowChars);
+  std::stringstream expectedVal;
+  expectedVal << std::source_location::current().file_name() << ":"
+              << (std::source_location::current().line() - 3) << " ";
+  auto startLen = strlen(expectedVal.str().c_str());
+  expectedVal << std::string(logLen - startLen - 1, 'a');
+
+  auto val = logger.remove_log();
+
+  ASSERT_TRUE(val.has_value());
+  EXPECT_STREQ(val->pBuffer(), expectedVal.str().c_str());
+  EXPECT_EQ(val->type(), LogType::INFO);
+
+  //   std::cout << val->pBuffer() << std::endl;
+  //   std::cout << expectedVal.str() << std::endl;
+}
+
+TEST(LoggerTest, LoggerOverflow) {
+  constexpr int nLogs = 10;
+  constexpr int logLen = 200;
+  Logger<nLogs, logLen> logger;
+  for (int i = 0; i <= nLogs; i++) {
+    logger.info("test");
+  }
+  // At this point, the second-to-last log should be the warning log
+  // And the log after is the log that caused the warning
+  for (int i = 0; i < nLogs - 2; i++) {
+    auto val = logger.remove_log();
+    ASSERT_TRUE(val.has_value());
+    EXPECT_EQ(val->type(), LogType::INFO) << "Iteration: " << i;
+    std::cout << val->pBuffer() << std::endl;
+  }
+  auto val = logger.remove_log();
+  ASSERT_TRUE(val.has_value());
+  EXPECT_EQ(val->type(), LogType::WARNING) << "First" << std::endl;
+  std::cout << val->pBuffer() << std::endl;
+  val = logger.remove_log();
+  ASSERT_TRUE(val.has_value());
+  EXPECT_EQ(val->type(), LogType::INFO) << "Second" << std::endl;
+  std::cout << val->pBuffer() << std::endl;
+}
