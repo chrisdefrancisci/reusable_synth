a7e89c1a2655ff5e7cc7a4efbaeac0aa4fe7899f
Chris DeFrancisci <chrisdefrancisci@gmail.com>
2026-03-19 11:02:58 -0400

    Modified mock_led to implement updated led interface

--- tests/mocks/mock_led.hpp
+++ tests/mocks/mock_led.hpp
@@ -6,5 +6,7 @@ class MockLed : public LedBase {
 public:
   MOCK_METHOD(void, on, (), (override));
   MOCK_METHOD(void, off, (), (override));
+  MOCK_METHOD(void, setIntensity, (int), (override));
+  MOCK_METHOD(void, setIntensity, (float), (override));
   MOCK_METHOD((std::pair<int, int>), getRange, (), (const, override));
 };
