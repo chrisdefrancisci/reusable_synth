2920a9abe56acda387251ade204a2c4c32e1b26d
Chris DeFrancisci <chrisdefrancisci@gmail.com>
2026-05-30 13:36:26 -0400

    multichannel DMA proof of concept, needs refactor

--- tests/tests/hardware/circular_dma_test.cpp
+++ tests/tests/hardware/circular_dma_test.cpp
@@ -17,58 +17,98 @@ static constexpr void mock_operation(std::span<T> out, T in)
     }
 }
 
-TEST(DacTest, WriteToDoubleBuffer)
+TEST(CircularDma, WriteToDoubleBuffer)
 {
-    constexpr int computationBufSize = 100;
-    constexpr int dacBufSize = computationBufSize * 2;
-    std::array<uint16_t, computationBufSize> computedData = { 0 };
-    std::array<uint16_t, dacBufSize> dacData = { 0 };
-    CircularDma dac(computedData, dacData);
+    constexpr int memBufSize = 100;
+    constexpr int periphBufSize = memBufSize * 2;
+    // Data manipulated in memory
+    std::array<uint16_t, memBufSize> memData = { 0 };
+    // Data for the peripheral
+    std::array<uint16_t, periphBufSize> periphData = { 0 };
+    CircularDma dma(memData, periphData);
 
     InterruptHandler fakeHalfCompleteCallback;
     InterruptHandler fakeCompleteCallback;
-    fakeHalfCompleteCallback.connect<&decltype(dac)::setHalfCompleteFlag>(&dac);
-    fakeCompleteCallback.connect<&decltype(dac)::setCompleteFlag>(&dac);
+    fakeHalfCompleteCallback.connect<&decltype(dma)::setHalfCompleteFlag>(&dma);
+    fakeCompleteCallback.connect<&decltype(dma)::setCompleteFlag>(&dma);
 
     // Before callbacks, all dacData should be 0
     uint16_t oldValue = 0;
-    EXPECT_THAT(dacData, testing::Each(oldValue));
-    EXPECT_TRUE(dac.isReady());
+    EXPECT_THAT(periphData, testing::Each(oldValue));
+    EXPECT_TRUE(dma.isReady());
     // (1) Do processing... copy data
     uint16_t newValue = 1;
-    mock_operation<uint16_t>(computedData, newValue);
-    dac.execute<DmaDirection::MemoryToPeripheral>();
-    EXPECT_FALSE(dac.isReady());
-    auto firstHalf = dacData | std::views::take(dacData.size() / 2);
-    auto lastHalf = dacData | std::views::drop(dacData.size() / 2);
+    mock_operation<uint16_t>(memData, newValue);
+    dma.execute<DmaDirection::MemoryToPeripheral>();
+    EXPECT_FALSE(dma.isReady());
+    auto firstHalf = periphData | std::views::take(periphData.size() / 2);
+    auto lastHalf = periphData | std::views::drop(periphData.size() / 2);
 
     EXPECT_THAT(firstHalf, testing::Each(testing::Eq(oldValue)));
     EXPECT_THAT(lastHalf, testing::Each(testing::Eq(newValue)));
 
     fakeHalfCompleteCallback();
-    EXPECT_TRUE(dac.isReady());
+    EXPECT_TRUE(dma.isReady());
 
     // (2) Do processing... copy data again
     oldValue = newValue;
     newValue = UINT16_MAX;
-    mock_operation<uint16_t>(computedData, newValue);
-    dac.execute<DmaDirection::MemoryToPeripheral>();
+    mock_operation<uint16_t>(memData, newValue);
+    dma.execute<DmaDirection::MemoryToPeripheral>();
 
-    EXPECT_FALSE(dac.isReady());
+    EXPECT_FALSE(dma.isReady());
     EXPECT_THAT(firstHalf, testing::Each(testing::Eq(newValue)));
     EXPECT_THAT(lastHalf, testing::Each(testing::Eq(oldValue)));
 
     fakeCompleteCallback();
-    EXPECT_TRUE(dac.isReady());
+    EXPECT_TRUE(dma.isReady());
 
     // (3) Do processing... copy data one last time
     oldValue = newValue;
     newValue = UINT16_MAX / 2;
-    mock_operation<uint16_t>(computedData, newValue);
-    dac.execute<DmaDirection::MemoryToPeripheral>();
-    EXPECT_FALSE(dac.isReady());
+    mock_operation<uint16_t>(memData, newValue);
+    dma.execute<DmaDirection::MemoryToPeripheral>();
+    EXPECT_FALSE(dma.isReady());
     EXPECT_THAT(firstHalf, testing::Each(testing::Eq(oldValue)));
     EXPECT_THAT(lastHalf, testing::Each(testing::Eq(newValue)));
 
     // fakeHalfCompleteCallback();
+}
+
+TEST(MultiChannelDma, Deinterleave) // PeripheralToMemory
+{
+    constexpr size_t nChannels = 10;
+    constexpr size_t channelBufSize = 100;
+    constexpr size_t memBufSize = nChannels * channelBufSize;
+    constexpr size_t periphBufSize = memBufSize * 2;
+    std::array<uint16_t, memBufSize> memData{};
+    std::array<uint16_t, periphBufSize> periphData{};
+
+    // Initialize the peripheral buffer
+    for (auto [idx, data] : std::views::enumerate(periphData)) {
+        data = idx % nChannels;
+    }
+
+    // This is what the DMA manager should do
+    for (auto i : std::views::iota(size_t(0), nChannels)) {
+        auto channelBuf = memData | std::views::drop(i * channelBufSize) |
+                          std::views::take(channelBufSize);
+        auto deinterleavedPeriphBuf =
+          periphData | std::views::drop(i) | std::views::stride(nChannels);
+        auto zipped = std::views::zip(channelBuf, deinterleavedPeriphBuf);
+        for (auto [out, in] : zipped) {
+            out = in;
+        }
+    }
+
+    for (auto i : std::views::iota(size_t(0), nChannels)) {
+        EXPECT_THAT(memData | std::views::drop(i * channelBufSize) |
+                      std::views::take(channelBufSize),
+                    testing::Each(testing::Eq(i)));
+    }
+}
+
+TEST(MultiChannelDma, Interleave) // MemoryToPeripheral
+{
+    EXPECT_TRUE(false);
 }
